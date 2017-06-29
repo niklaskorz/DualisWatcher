25f903e9897d63a8028393636619774f64d5ba0b
Niklas Korz <korz.niklask@gmail.com>
2017-06-29 17:50:49 +0200

    Implement shooter and parts of service

--- notification_services/pushbullet/pushbullet_service.py
+++ notification_services/pushbullet/pushbullet_service.py
@@ -1,4 +1,7 @@
+import logging
+import traceback
 from notification_services.notification_service import NotificationService
+from notification_services.pushbullet.pushbullet_shooter import PushbulletShooter
 
 class PushbulletService(NotificationService):
     def interactively_configure(self):
@@ -16,4 +19,42 @@ class PushbulletService(NotificationService):
                 access_token = input('Your pushbullet access token:   ')
 
                 print('Testing pushbullet config...')
-                
+                p = PushbulletShooter(access_token)
+                config_valid = p.verify()
+                if not config_valid:
+                    print('Invalid access token. Please try again.')                
+                    continue
+            
+                pushbullet_cfg = {
+                    'access_token': access_token
+                }
+                self.config_helper.set_property('pushbullet', pushbullet_cfg)
+    
+    def _send_push(self, title: str, body: str) -> bool:
+        try:
+            pushbullet_cfg = self.config_helper.get_property('pushbullet')
+        except ValueError:
+            logging.debug('Pushbullet notifications not configured, skipping.')
+            return False
+        
+        try:
+            logging.debug('Sending notification with pushbullet...')
+            pushbullet_shooter = PushbulletShooter(pushbullet_cfg['access_token'])
+            pushbullet_shooter.send(title, body)
+            pushbullet_shooter.close()
+        except:
+            error_formatted = traceback.format_exc()
+            logging.error('While sending notification:\n%s' % (error_formatted), extra={'exception': e})
+            pass  # ignore the exception further up
+    
+    def notify_about_changes_in_results(self, changes: CollectionOfChanges, course_names: {str: str}, token: str):
+        mail_content = create_full_dualis_diff_mail(changes, course_names, token)
+        self._send_mail('%s neue Änderungen in den Modul-Ergebnissen!'%(changes.diff_count), mail_content)
+
+    def notify_about_changes_in_schedule(self, changes: [str], uid: str):
+        mail_content = create_full_schedule_diff_mail(changes, uid)
+        self._send_mail('%s neue Änderungen im Vorlesungsplan!'%(len(changes)), mail_content)
+
+    def notify_about_error(self, error_description: str):
+        mail_content = create_full_error_mail(error_description)
+        self._send_mail('Fehler!', mail_content)
--- notification_services/pushbullet/pushbullet_shooter.py
+++ notification_services/pushbullet/pushbullet_shooter.py
@@ -1,8 +1,46 @@
+import json
+from http.client import HTTPSConnection, HTTPResponse
+
 class PushbulletShooter(object):
     """
     Encapsulates the sending of pushbullet pushes.
     """
     def __init__(self, access_token: str):
-        self.access_token = access_token
+        self.connection = HTTPSConnection('api.pushbullet.com')
+        self.token = access_token
+    
+    def close(self):
+        self.connection.close()
 
+    def verify(self):
+        self.connection.request(
+            'GET',
+            '/v2/users/me',
+            headers={
+                'Access-Token': self.token
+            }
+        )
+
+        response = self.connection.getresponse()
+        return response.getcode() == 200
+
+    def send(self, data):
+        self.connection.request(
+            'POST',
+            '/v2/pushes',
+            body=json.dumps(data),
+            headers={
+                'Access-Token': self.token,
+                'Content-Type': 'application/json'
+            }
+        )
+
+        response = self.connection.getresponse()
+        status = response.getcode()
+        body = response.read().decode('utf-8')
+        print(body)
+        return status, json.loads(body)
+
+    def send_note(self, title, body):
+        self.send({ 'type': 'note', 'title': title, 'body': body })
     
