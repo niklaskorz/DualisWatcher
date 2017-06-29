91ee39a59f9ed192da5b98cc2bc1ce6e65e1d565
Niklas Korz <korz.niklask@gmail.com>
2017-06-29 23:41:14 +0200

    Update pb service

--- notification_services/pushbullet/pushbullet_service.py
+++ notification_services/pushbullet/pushbullet_service.py
@@ -6,14 +6,12 @@ from notification_services.pushbullet.pushbullet_shooter import PushbulletShoote
 class PushbulletService(NotificationService):
     def interactively_configure(self):
         do_pushbullet_input = input('Do you want to activate notifications via pushbullet? [y/n]   ')
-        while not (do_pushbullet_input == 'y' or do_pushbullet_input = 'n')
+        while not (do_pushbullet_input == 'y' or do_pushbullet_input == 'n'):
             do_pushbullet_input = input('Unrecognized input. Try again:   ')
         
         if do_pushbullet_input == 'n':
             self.config_helper.remove_property('pushbullet')
         else:
-            print('[The following Inputs are not validated!]')
-
             config_valid = False
             while not config_valid:
                 access_token = input('Your pushbullet access token:   ')
